d85a6f8206b1b73644a7c9680548a0a0a1208252
Luis de Bethencourt <luisbg@osg.samsung.com>
2016-12-04 19:50:59 +0000

    Use error-chain
    https://github.com/brson/error-chain/

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,43 @@
-extern crate rustbox;
+// `error_chain!` can recurse deeply
+#![recursion_limit = "1024"]
 
-use std::error::Error;
+extern crate rustbox;
+#[macro_use] extern crate error_chain;
+
+use std::error::{Error as StdError};
 use std::default::Default;
 
 use rustbox::{Color, RustBox, OutputMode};
 use rustbox::Key;
 
+mod errors {
+    error_chain! { }
+}
+
+use errors::Result;
+
 fn main() {
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+            for e in e.iter().skip(1) {
+                println!("caused by: {}", e);
+            }
+
+        // The backtrace is not always generated. Try to run this example
+        // with `RUST_BACKTRACE=1`.
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}",
+                     backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
     let mut rustbox = match RustBox::init(Default::default()) {
-        Result::Ok(v) => v,
-        Result::Err(e) => panic!("{}", e),
+        Ok(v) => v,
+        Err(e) => bail!("{}", e),
     };
 
     rustbox.set_output_mode(OutputMode::EightBit);
@@ -28,8 +56,10 @@ fn main() {
                     _ => { }
                 }
             },
-            Err(e) => panic!("{}", e.description()),
+            Err(e) => bail!("Rustbox.poll_event Error {}", e.description()),
             _ => { }
         }
     }
+
+    Ok(())
 }
